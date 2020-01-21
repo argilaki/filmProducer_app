149e59af4cb825a87f53c8901c652d4d9dff337b
ar.gilaki <ar.gilaki@gmail.com>
2020-01-21 22:16:15 +0330

    using my function to check logging

--- blog.py
+++ blog.py
@@ -10,8 +10,8 @@ DATABASE = 'blog.db'
 USERNAME = 'abc'
 PASSWORD = 'abc'
 SECRET_KEY = 'ABC'
-
-app = Flask(__name__,template_folder='templates')
+flag = 0
+app = Flask(__name__)
 # pulls in configurations by looking for UPPERCASE variables
 app.config.from_object(__name__)
 # function used for connecting to the database
@@ -28,11 +28,22 @@ def login():
             status_code = 401
         else:
             session['logged_in'] = True
+            flag = 1
             return redirect(url_for('main'))
     return render_template('login.html', error=error), status_code
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    flag = 0
+    flash('you previously logged out')
+    return redirect(url_for('login'))
+
 @app.route('/main')
 def main():
-    return render_template('main.html')
+    if 'logged_in' in session:
+        return render_template('main.html')
+    flash('you didn\'t login')
+    return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run(debug=True)
